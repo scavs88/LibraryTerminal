4dad8e2f1b39a62fb1dddcba067493b5e04ae99b
kalaiarasy <skalaiarasy@gmail.com>
2021-04-23 15:26:24 -0400

    Library class added

---
+++ LibraryTerminal/Library.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryTerminal
+{
+    class Library
+    {
+        //field
+        private List<Book> _book = new List<Book>
+        {
+            new Book("Dr.Suess","The cat in the Hat",true,""),
+            new Book("Jhumpa Lahiri", "Interpreter of Maladies" , true ,""),
+            new Book("Jim Harrison", "Woman Lit by Fireflies", false, "5/14/21")
+        };
+        //property
+        public List<Book> Book
+        {
+            get { return this._book; }
+            set { this._book = value; }
+        }
+        //Constructor
+        public Library()
+        {
+
+        }
+
+        //methods
+
+        public void DisplayList()
+        {
+            foreach(Book books in this._book)
+            {
+                Console.WriteLine(books.ToString());
+            }
+        }
+
+    }
+}
