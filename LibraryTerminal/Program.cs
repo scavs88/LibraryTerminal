7e1350a17a461f8d1472244b02c32e22551c150c
kalaiarasy <skalaiarasy@gmail.com>
2021-04-23 18:58:51 -0400

    Few changes in main program

--- LibraryTerminal/Program.cs
+++ LibraryTerminal/Program.cs
@@ -10,11 +10,34 @@ namespace LibraryTerminal
         {
             Library library = new Library();
             library.DisplayList();
-            Console.WriteLine("Enter the author  name to search");
-            string name = Console.ReadLine();
-            bool check1 = library.SearchByAuthor(name);
-            Console.WriteLine(check1);
-            //bool
+            Console.WriteLine();
+            Console.WriteLine("Enter your choice name/title");
+            string choice = Console.ReadLine();
+            if (choice == "name")
+            {
+                Console.WriteLine("Enter the author  name to search");
+                string name = Console.ReadLine();
+                bool check1 = library.SearchByAuthor(name);
+                Console.WriteLine(check1);
+                if (check1 == true)
+                {
+                    Console.WriteLine("Do you want to check out?");
+                    string respond = Console.ReadLine().ToLower().Trim();
+                    if(respond == "y")
+                    {
+                        Console.WriteLine($"your due date is:");
+                        library.CreateDueDate();
+                    }
+                    else
+                    {
+
+                    }
+                }
+
+            }
+
+            
+            
 
             //Book book = new Book("Jim Harrison", "Woman Lit by Fireflies", false, 14);
             //Console.WriteLine(book);
