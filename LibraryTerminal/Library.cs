546445008650d2d6802936ffac56bc7db21fa94d
sean <spcavanaugh88@gmail.com>
2021-04-25 09:34:21 -0400

    sean commit

--- LibraryTerminal/Library.cs
+++ LibraryTerminal/Library.cs
@@ -38,12 +38,60 @@ namespace LibraryTerminal
             }
         }
 
+
+        public void KeywordSearch()
+        {
+            foreach (Book word in title)
+            {
+                bool keyword = true;
+                while (keyword)
+                if (word == Library.)
+                    {
+
+                    }
+            }
+        }
+
+
         public void CreateDueDate()
         {
             var date = DateTime.Now.AddDays(14); 
             var dateShort = date.ToShortDateString();
             Console.WriteLine(dateShort);
         }
+
+        public  bool SearchByAuthor(string input)
+        {
+            bool status = false;
+            foreach(Book book in this._book)
+            {
+                if (book.Author == input)
+                {
+                    Console.WriteLine("Yes the book is avaiable");
+                    status= true;
+                }
+
+            }
+            return status;
+        }
+
+        public bool SearchByTitle(string input)
+        {
+            bool status = false;
+            foreach (Book book in this._book)
+            {
+                if (book.Title == input)
+                {
+                    Console.WriteLine("Yes the book is avaiable");
+                    status = true;
+                }
+
+            }
+            return status;
+        }
+
+        //public void 
+
 
     }
 }
--- LibraryTerminal/Program.cs
+++ LibraryTerminal/Program.cs
@@ -11,50 +11,14 @@ namespace LibraryTerminal
 
             Console.WriteLine("Welcome to the library");            
             Library library = new Library();
-            
-            //prompt to search here
-            while (true)
-            {
-                Console.WriteLine("Please select a search function");
-                Console.WriteLine("To see our entire inventory enter 1");
-                Console.WriteLine("To search by author enter 2");
-                Console.WriteLine("To search by title enter 3");
-                int choice = int.Parse(Console.ReadLine());
-                if (choice == 1)
-                {
-                    library.DisplayList();
-                    break;
-                }
-                else if (choice == 2)
-                {
-                    //implement author search method here
-                    break;
-                }else if (choice == 3)
-                {
-                    //implement title search
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input.");
-                }
-            }
-
-
-
-
-
-            //ask for a selection here
-
-
-
-
+            library.DisplayList();
             //Book book = new Book("Jim Harrison", "Woman Lit by Fireflies", false, 14);
             //Console.WriteLine(book);
 
             //StreamWriter writer = new StreamWriter("../../../testfile.txt");
             //writer.WriteLine("Hi test");
             //writer.Close();
-            
+            library.CreateDueDate();
         }
     }
 }
