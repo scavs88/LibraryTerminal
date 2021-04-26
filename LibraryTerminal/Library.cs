969f6da516f283b47b13db3e42a12cffdf0e122a
Jason <j_schubb53@yahoo.com>
2021-04-26 11:05:47 -0400

    Added keyword method and title list

--- LibraryTerminal/Library.cs
+++ LibraryTerminal/Library.cs
@@ -11,10 +11,17 @@ namespace LibraryTerminal
         {
             new Book("Dr.Suess","The cat in the Hat",true,""),
             new Book("Jhumpa Lahiri", "Interpreter of Maladies" , true ,""),
-            new Book("Jim Harrison", "Woman Lit by Fireflies", false, "5/14/21")
-
+            new Book("Jim Harrison", "Woman Lit by Fireflies", false, "5/14/2021"),
+            new Book("Leslie Orchard", "Hacking RSS and Atom", true, ""),
+            new Book("Christian Klaver", "Shadows Over London", true, ""),
+            new Book("Esther Averil", "Jenny and the Cat Club", false,  "5/7/2021"),
+            new Book("Anthony Bourdain", "Kitchen Confidential", false, "5/8/2021"),
+            new Book("Grant Achatz", "Life on the Line", true, ""),
+            new Book("Bill Bryson", "Walk in the Woods", false, "5/10/2021"),
+            new Book("William Shakespeare", "Comedy of Errors", true, ""),
+            new Book("Elizabeth Haydon", "Rhapsody", true, ""),
+            new Book("John Feinstein", "Last Amateurs", true, ""),
         };
-
 
         //property
         public List<Book> Books
@@ -53,14 +60,14 @@ namespace LibraryTerminal
             foreach (Book book in this._books)
             {
                 if (book.Author == input)
-                {                    
+                {
                     status = true;
                     Console.WriteLine(book);
                 }
             }
             if (!status)
             {
-                    Console.WriteLine(" That book is not avaiable");                
+                Console.WriteLine(" That book is not avaiable");
             }
             return status;
         }
@@ -103,17 +110,24 @@ namespace LibraryTerminal
                     status = true;
                 }
             }
-           
+
         }
 
-      
-
-
-        
-
+        public void SearchByKeyword(string input)
+        {
+            foreach (Book word in this._books)
+            {
+                if (word.Title == input)
+                {
+                    Console.WriteLine($"We found {word.ToString()}");
+                }
+                else
+                {
+                    Console.WriteLine($"There was no book with {word} in any title.");
+                }
+            }
+        }
     }
-
-
 }
 
 
