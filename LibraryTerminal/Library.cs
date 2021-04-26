815e5fbbed910b7278fde0761819339f48b26e8f
sean <spcavanaugh88@gmail.com>
2021-04-26 09:08:35 -0400

    updated logic for main method

--- LibraryTerminal/Library.cs
+++ LibraryTerminal/Library.cs
@@ -7,7 +7,7 @@ namespace LibraryTerminal
     class Library
     {
         //field
-        private List<Book> _book = new List<Book>
+        private List<Book> _books = new List<Book>
         {
             new Book("Dr.Suess","The cat in the Hat",true,""),
             new Book("Jhumpa Lahiri", "Interpreter of Maladies" , true ,""),
@@ -17,10 +17,10 @@ namespace LibraryTerminal
 
         
         //property
-        public List<Book> Book
+        public List<Book> Books
         {
-            get { return this._book; }
-            set { this._book = value; }
+            get { return this._books; }
+            set { this._books = value; }
         }
         //Constructor
         public Library()
@@ -32,30 +32,30 @@ namespace LibraryTerminal
 
         public void DisplayList()
         {
-            foreach(Book books in this._book)
+            foreach(Book books in this._books)
             {
                 Console.WriteLine(books.ToString());
             }
         }
 
 
-        public void KeywordSearch()
-        {
-            foreach (Book word in title)
-            {
-                bool keyword = true;
-                while (keyword)
-                if (word == Library.)
-                    {
+        //public void KeywordSearch()
+       // {
+          //  foreach (Book word in this._title)
+          //  {
+           //     bool keyword = true;
+              //  while (keyword)
+            //    if (word == Library.)
+           //         {
 
-                    }
-            }
-        }
+           //         }
+          //  }
+      //  }
 
 
         public void CreateDueDate()
         {
-            var date = DateTime.Now.AddDays(14); 
+            var date = DateTime.Now.AddDays(14);            
             var dateShort = date.ToShortDateString();
             Console.WriteLine(dateShort);
         }
@@ -63,11 +63,11 @@ namespace LibraryTerminal
         public  bool SearchByAuthor(string input)
         {
             bool status = false;
-            foreach(Book book in this._book)
+            foreach(Book book in this._books)
             {
                 if (book.Author == input)
                 {
-                    Console.WriteLine("Yes the book is avaiable");
+                    Console.WriteLine($"We have these titles by {input}, {book.Title}");
                     status= true;
                 }
 
@@ -78,7 +78,7 @@ namespace LibraryTerminal
         public bool SearchByTitle(string input)
         {
             bool status = false;
-            foreach (Book book in this._book)
+            foreach (Book book in this._books)
             {
                 if (book.Title == input)
                 {
@@ -89,6 +89,39 @@ namespace LibraryTerminal
             }
             return status;
         }
+
+        //public void 
+
+
+        //public  bool SearchByAuthor(string input)
+       // {
+         //   bool status = false;
+         //   foreach(Book book in this._books)
+         //   {
+             //   if (book.Author == input)
+            //    {
+            //        Console.WriteLine("Yes the book is avaiable");
+              //      status= true;
+              //  }
+
+           // }
+          //  return status;
+        //}
+
+       // public bool SearchByTitle(string input)
+       // {
+          //  bool status = false;
+           // foreach (Book book in this._books)
+          //  {
+            //    if (book.Title == input)
+            //    {
+               //     Console.WriteLine("Yes the book is avaiable");
+               //     status = true;
+              //  }
+
+         //   }
+          //  return status;
+       // }
 
         //public void 
 
--- LibraryTerminal/Program.cs
+++ LibraryTerminal/Program.cs
@@ -11,14 +11,60 @@ namespace LibraryTerminal
 
             Console.WriteLine("Welcome to the library");            
             Library library = new Library();
-            library.DisplayList();
+            
+
+            Console.WriteLine("Please select a menu item");
+            Console.WriteLine("1.) Display library inventory.");
+            Console.WriteLine("2.) Search for a book by author");
+            Console.WriteLine("3.) Search for a book by title");
+            Console.WriteLine("4.) Return a book");          
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                library.DisplayList();
+            }
+            else if  (choice == "2")
+            {
+                Console.WriteLine("Enter the author name to search");
+                string name1 = Console.ReadLine();
+                bool check1 = library.SearchByAuthor(name1);
+                Console.WriteLine(check1);
+                if (check1 == true)
+                {
+                    Console.WriteLine("Do you want to check this book out? y/n");
+                    string respond = Console.ReadLine().ToLower().Trim();
+                    if (respond == "y")
+                    {
+                        Console.WriteLine($"your due date is:");
+                        library.CreateDueDate();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"We do not currently have any books by {name1}");
+                    }
+                }
+
+            }
+
+            
+            
+
+
+           
+
+
+            //Console.WriteLine("Enter the author  name to search");
+            //string name = Console.ReadLine();
+            //bool check1 = library.SearchByAuthor(name);
+            //Console.WriteLine(check1);
+            //bool
             //Book book = new Book("Jim Harrison", "Woman Lit by Fireflies", false, 14);
             //Console.WriteLine(book);
-
             //StreamWriter writer = new StreamWriter("../../../testfile.txt");
             //writer.WriteLine("Hi test");
             //writer.Close();
-            library.CreateDueDate();
+            //library.CreateDueDate();
+
         }
     }
 }
